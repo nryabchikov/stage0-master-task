d955e7349a2428f1022d183b5a5a9ec94eb92ab0
Nekitos4 <ryabchikovnik241104@gmail.com>
2023-10-27 17:38:33 +0300

    feat: conditions

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,43 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+        if (year > 0 && (month >= 1 && month <= 12)) {
+            if (year % 400 == 0) {
+                if (month == 2) {
+                    System.out.println(29);
+                } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                    System.out.println(30);
+                } else {
+                    System.out.println(31);
+                }
+            } else if (year % 100 == 0) {
+                if (month == 2) {
+                    System.out.println(28);
+                } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                    System.out.println(30);
+                } else {
+                    System.out.println(31);
+                }
+            } else if (year % 4 == 0) {
+                if (month == 2) {
+                    System.out.println(29);
+                } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                    System.out.println(30);
+                } else {
+                    System.out.println(31);
+                }
+            } else {
+                if (month == 2) {
+                    System.out.println(28);
+                } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                    System.out.println(30);
+                } else {
+                    System.out.println(31);
+                }
+            }
+        } else {
+            System.out.println("invalid date");
+        }
     }
 
 }
--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -3,6 +3,19 @@ package com.epam.conditions;
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+        String result;
+        if (monthNumber == 12 || monthNumber == 1 || monthNumber == 2) {
+            result = "Winter";
+        } else if (monthNumber > 2 && monthNumber < 6) {
+            result = "Spring";
+        } else if (monthNumber > 5 && monthNumber < 9) {
+            result = "Summer";
+        } else if (monthNumber > 8 && monthNumber < 12) {
+            result = "Autumn";
+        } else {
+            result = "Wrong month number";
+        }
+        System.out.println(result);
     }
 
 }
